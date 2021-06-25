331f3aca7dd9284346ef200950df555eae058a92
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-06-25 00:34:41 -0400

    Revert "trying to change php code"
    This reverts commit 09e4f1169bdbd9c94c76f39eaeecd2800cc83275.

--- mail.php
+++ mail.php
@@ -1,18 +1,56 @@
 <?php
-    $to = "noahschristopher250@gmail.com";
-    $subject = "This is subject";
+$name    = (isset($_POST['name'])) ? $_POST['name'] : '';
+$email   = (isset($_POST['email'])) ? $_POST['email'] : '';
+$subject = (isset($_POST['subject'])) ? $_POST['subject'] : '';
+$message = (isset($_POST['message'])) ? $_POST['message'] : '';
 
-    $message = "<b>This is HTML message.</b>";
-    $message .= "<h1>This is headline.</h1>";
+header('Content-Type: application/json');
+if ($name === '') {
+  $response =  json_encode(array(
+    'message' => 'Name cannot be empty',
+    'code' => 0
+  ));
+  die($response);
+}
+if ($email === '') {
+  $response =  json_encode(array(
+    'message' => 'Email cannot be empty',
+    'code' => 0
+  ));
+  die($response);
+} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $response =  json_encode(array(
+    'message' => 'Email format invalid.',
+    'code' => 0
+  ));
+  die($response);
+}
+if ($subject === '') {
+  $response =  json_encode(array(
+    'message' => 'Subject cannot be empty',
+    'code' => 0
+  ));
+  die($response);
+}
+if ($message === '') {
+  $response =  json_encode(array(
+    'message' => 'Message cannot be empty',
+    'code' => 0
+  ));
+  die($response);
+}
 
-    $header = "From:mymailid@gmail.com \r\n";
-    $retval = mail($to,$subject,$message,$header);
-    if(isset($retval))//change
-    {
-        echo "Message sent successfully...";
-    }
-    else
-    {
-        echo "Message could not be sent...";
-    }
+$content    = "From: $name \n Email: $email \n Message: $message";
+$recipient  = "info@midlandpaymentsservices.com";
+
+$mailheader = array(
+  'From' => $email,
+  'Content-Type' => 'text/html;charset=UTF-8',
+);
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+$response =  json_encode(array(
+  'message' => 'Email successfully sent!',
+  'code' => 1
+));
+die($response);
 ?>
