ea76d5520d303911f61fb3ebe155d238ea7e0502
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-06-29 14:17:40 -0400

    back

--- about.php
+++ about.php
@@ -1,15 +1,8 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
-<<<<<<< HEAD
-<<<<<<< HEAD
     $mail->SMTPDebug = 2;
-    $mail = new PHPMailer(true);
-=======
-=======
->>>>>>> parent of 06aba96 (debug)
     $mail = new PHPMailer;
->>>>>>> parent of 06aba96 (debug)
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->Port = 465;
@@ -178,15 +171,7 @@ EOT;
                 </button>
 
                 <?php if (!empty($msg)) {
-<<<<<<< HEAD
-<<<<<<< HEAD
                   echo "<h4>$msg</h4>";
-=======
-                  echo "<h2>$msg</h2>";
->>>>>>> parent of 06aba96 (debug)
-=======
-                  echo "<h2>$msg</h2>";
->>>>>>> parent of 06aba96 (debug)
                 } ?>
 
               </form>
--- form.php
+++ form.php
@@ -1,6 +1,7 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
+    $mail->SMTPDebug = 2;
     $mail = new PHPMailer;
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
