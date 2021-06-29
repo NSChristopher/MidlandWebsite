ac44c5becf301d14553e5e65532e3f85cf0c6a43
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-06-29 13:24:36 -0400

    tweak

--- about.php
+++ about.php
@@ -1,3 +1,37 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 465;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'platinumpayrol@gmail.com';
+    $mail->Password = 'PLAT1007';
+    $mail->setFrom('noahschristopher250@gmail.com', 'Mr. Noah');
+    $mail->addAddress('noahschristopher250@gmail.com', 'Receiver Name');
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'PHPMailer contact form';
+        $mail->isHTML(false);
+        $mail->Body = <<<EOT
+Email: {$_POST['email']}
+Name: {$_POST['name']}
+Message: {$_POST['message']}
+EOT;
+        if (!$mail->send()) {
+            $msg = 'Sorry, something went wrong. Please try again later.';
+        } else {
+            $msg = 'Message sent! Thanks for contacting us.';
+        }
+    } else {
+        $msg = 'Share it with us!';
+    }
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -108,7 +142,7 @@
 
             <div class="col-md-6 mb-4">
 
-              <form name="form" action="form.php" method="POST">
+              <form name="form" method="POST">
                 <h2>Contact us</h2>
 
                 <div class="form-outline mb-4">
