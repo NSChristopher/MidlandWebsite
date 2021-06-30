21a143006212fc9ccb7064411a118d854682e62c
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-06-30 13:53:57 -0400

    dn

--- about.php
+++ about.php
@@ -155,6 +155,7 @@
                   <input
                     type="text" 
                     name="subject"
+                    class="form-control"
                   />
                   <label class="form-label" for="subject">Subject</label>
                 </div>
@@ -164,6 +165,7 @@
                     type="text"
                     name="message" 
                     rows="4"
+                    class="form-control"
                   ></textarea>
                   <label class="form-label" for="message">Message</label>
                 </div>
