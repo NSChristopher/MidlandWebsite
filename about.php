4f53dbc2f25d41347fd358cc02bde6dde0b36d6c
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-06-30 14:16:16 -0400

    remove id tag

--- about.php
+++ about.php
@@ -28,10 +28,10 @@
     $(document).ready(function () {
     $("form").submit(function (event) {
       var formData = {
-        name: $("#name").val(),
-        email: $("#email").val(),
-        subject: $("#subject").val(),
-        message: $("#message").val(),
+        name: $("name").val(),
+        email: $("email").val(),
+        subject: $("subject").val(),
+        message: $("message").val(),
       };
   
       $.ajax({
