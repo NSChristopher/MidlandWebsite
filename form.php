76608c1d56a07c7db2df3ed693e32939be9682ad
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-07-01 18:55:02 -0400

    changed success message

--- form.php
+++ form.php
@@ -24,7 +24,7 @@ if (!empty($errors)) {
     $data['errors'] = $errors;
 } else {
     $data['success'] = true;
-    $data['message'] = 'Success!';
+    $data['message'] = 'Email sent!';
 }
 
 echo json_encode($data);
