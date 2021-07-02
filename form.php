f9533cc6de9b2f51b622a10d6542a10f0141cd4e
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-07-01 20:30:02 -0400

    added phpmailer

--- form.php
+++ form.php
@@ -1,5 +1,18 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->SMTPDebug = 2;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAutoTLS = false;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+
+
+
 $errors = [];
 $data = [];
 
@@ -25,6 +38,27 @@ if (!empty($errors)) {
 } else {
     $data['success'] = true;
     $data['message'] = 'Email sent!';
+}
+
+$mail->Username = 'platinumpayrol@gmail.com';
+$mail->Password = 'uetsnvrihifjbdcc';
+$mail->setFrom('noah@cardpaymentsolutions.com', 'Gus Prentzas');
+$mail->addAddress('noahschristopher250@gmail.com', 'Receiver Name');
+if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+    $mail->Subject = $_POST['subject'] . ' | Website Contact';
+    $mail->isHTML(false);
+    $mail->Body = <<<EOT
+    Email: {$_POST['email']}
+    Name: {$_POST['name']}
+    Message: {$_POST['message']}
+    EOT;
+    if (!$mail->send()) {
+        $msg = 'Sorry there was an issue. please try again later.';
+    } else {
+        $msg = 'Message sent! Thanks for contacting us.';
+    }
+} else {
+    $msg = 'Share it with us!';
 }
 
 echo json_encode($data);
