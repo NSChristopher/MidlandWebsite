ad4bd94c990a13f238687d054b05323c1a7a8254
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-07-04 08:50:12 -0400

    test

--- mailer.php
+++ mailer.php
@@ -1,48 +1,10 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-require 'vendor/autoload.php';
 
 $data = [];
-$mail = new PHPMailer();
-
-$mail->IsSMTP();
-$mail->SMTPDebug = 2;
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAutoTLS = false;
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-
-$mail->Username = 'platinumpayrol@gmail.com';
-$mail->Password = 'uetsnvrihifjbdcc';
 
 $data['success'] = true;
 $data['message'] = 'Share it with us!';
 echo json_encode($data);
 
-$mail->setFrom('noah@cardpaymentsolutions.com', 'Gus Prentzas');
-$mail->addAddress('noahschristopher250@gmail.com', 'Receiver Name');
-if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
-    $mail->Subject = $_POST['subject'] . ' | Website Contact';
-    $mail->isHTML(false);
-    $mail->Body = <<<EOT
-    Email: {$_POST['email']}
-    Name: {$_POST['name']}
-    Message: {$_POST['message']}
-    EOT;
-    if (!$mail->send()) {
-        $data['success'] = true;
-        $data['message'] = 'Sorry there was an issue. please try again later.';
-    } else {
-        $data['success'] = true;
-        $data['message']  = 'Message sent! Thanks for contacting us.';
-    }
-} else {
-    $data['success'] = true;
-    $data['message'] = 'Share it with us!';
-}
 
-$data['success'] = true;
-$data['message'] = 'Share it with us!';
-echo json_encode($data);
 ?>
