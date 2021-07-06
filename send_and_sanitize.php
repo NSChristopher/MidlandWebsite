67ff6c9fd46b0a747f1ed04f0724ff03dd0c3394
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-07-05 22:53:50 -0400

    remove new instance of mailFunc

--- send_and_sanitize.php
+++ send_and_sanitize.php
@@ -4,8 +4,6 @@ header('Content-type: application/json');
 $response['success'] = true;
 $response['error'] = null;
 
-$func = new mailFunc();
-
 try {
     $func->sanitize_and_validate();
     $func->send_mail();
