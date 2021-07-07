fa52943ba20cb79e8678469948e07e414841db42
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-07-07 03:12:06 -0400

    test

--- send_and_sanitize.php
+++ send_and_sanitize.php
@@ -18,8 +18,8 @@ $func = new mailFunc();
 $data = $func->sanitize_and_validate($data);
 
 if ($data['success']) {
-    $data = $func->send_mail($data);
     echo json_encode($data);
+    $data = $func->send_mail($data);
 }
 
 echo json_encode($data);
