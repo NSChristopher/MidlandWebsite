0b1cd107813793d72841e38b105f923c4ccf58d7
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-07-07 03:59:45 -0400

    tw

--- about.php
+++ about.php
@@ -160,18 +160,18 @@
 
               <br class="my-4">
 
-            </div>
-          </div>
-
-          <h3 class="mb-4">Give us a call</h3>
+              <h3 class="mb-4">Give us a call</h3>
           <div class="row">
-            <div class="col-auto">
+            <div class="col-12">
               <p>Sales: (917) 254-6874</p>
             </div>
-            <div class="col-auto">
+            <div class="col-12">
               <a href="tel:9172546874">
                 <button type="button" class="btn btn-warning btn-rounded me-sm-3 mb-4"><i class="fas fa-phone-alt"></i> Call Now</button>
               </a>
+            </div>
+          </div>
+
             </div>
           </div>
 
